95ec35302ed78f62e827511d86f13bbd8338afb3
JOHN <jdante1989@gmail.com>
2024-01-20 09:48:10 -0500

    welcome to my conuntry

--- welcome.py
+++ welcome.py
@@ -1,6 +1,9 @@
 name = "John"
 country = "Colombia"
+birthday = 1989
+now = 2024
 
 print("You're", name, "and you're from", country)
 print("You're", name)
 print("You're from", country)
+print("You are age is", now-birthday)
