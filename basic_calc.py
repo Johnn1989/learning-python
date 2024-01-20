f329ffafb16edaba0b29396f3f1d99774369b571
JOHN <jdante1989@gmail.com>
2024-01-20 09:24:43 -0500

    First commit basic_calc script: Features Calc v.1.0 and v.1.1

---
+++ basic_calc.py
@@ -0,0 +1,35 @@
+#Script title: Basic calculator v1
+'''Developer name: John E. Chavez
+Date: 18-01-2024 '''
+
+'''Description:
+This calculator let you get the result of 4 basic
+ math operations from two numbers (variables)
+'''
+
+#INPUTS (two varibles)
+num1 = 20
+num2 = 5
+
+#PROCESS
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+#OUTPUTS
+print(":::BASIC CALC V1:::")
+print("Number 1:", num1, type(num1))
+print("Number 2:", num2, type(num2))
+print ("The addition is: ", add, type(add))
+print ("The substraction is: ", sub, type(sub))
+print ("The multiplication is: ", mul, type(mul))
+print ("The divition is: ", div, type(div))
+print ("\n")
+print(":::BASIC CALC V1:::")
+print("Number 1:", num1, type(num1))
+print("Number 2:", num2, type(num2))
+print ("The addition is: ", add, type(add))
+print ("The substraction is: ", sub, type(sub))
+print ("The multiplication is: ", mul, type(mul))
+print ("The divition is: ", div, type(div))
