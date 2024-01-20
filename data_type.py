509f4378ed950bd4bbf68a4ae45023763b5fb279
JOHN <jdante1989@gmail.com>
2024-01-20 12:09:57 -0500

    mensaje

---
+++ data_type.py
@@ -0,0 +1,37 @@
+#Tipos de datos en Python
+#Python es una lenguaje dinamicamente tipado
+#1. Numericos.
+##Enteros z
+num1 = 42
+print("num1: ", type(num1))
+##Flotantes o Decimales (Reales)
+num2 = 50.5
+print("num2: ", type(num2))
+##Complejos => Complex
+num3 = 2j
+print("num3: ", type(num3))
+
+#2. Cadena => String => str
+my_name = "John"
+my_lastname = 'Erazo'
+profile = '''
+John es una persona enfocada 
+en el trabajo de equipos'''
+addres = 'Diagonal 14 A # 10 E 74/Barrio el Rosario'
+telephone = 3217796604
+print("profile", type(profile))
+print("addres", type(addres))
+print("telephone", type(telephone))
+#3. Logicos. (Valores o expresiones booleanas)
+usuario_activo = true
+print()
+#4. Listas.
+frutas = ['apple', 'banana']
+#5. Tuplas.
+#6. Diccionarios.
+#7. Conjuntos.
+
+a = 1
+b = 1 
+suma1 = a + b
+print("suma1 :", suma1, suma1)
