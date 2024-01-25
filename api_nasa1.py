1ef0e5a9c2d9fad6a0f4f1881a9c4b04f13891f4
JOHN <jdante1989@gmail.com>
2024-01-24 20:33:55 -0500

    script acerca de la NASA

---
+++ api_nasa1.py
@@ -0,0 +1,11 @@
+'''API: Application Programming interface
+Nasa API: https: //api.nasa.gov/
+API_KEY_NASA: a8u4kO1Kt9vFlS8FzbGtJbUixhOzZu38xl7LzjBq
+Developer: John J Erazo
+Date: 24012024
+Scrip description: Get and read data from NASA API about comets
+'''
+
+
+import requests
+import pygame
