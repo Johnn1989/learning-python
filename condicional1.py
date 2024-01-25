da0535ac44b6ae23b0200d09ac34870ac74689d3
JOHN <jdante1989@gmail.com>
2024-01-24 19:56:40 -0500

    scrip los condicionales

---
+++ condicional1.py
@@ -0,0 +1,21 @@
+#Los condicionales 
+'''
+pueden ser simples o compuestos / anidados
+Es necesario usar tabla de operadores logicos:
+> => Mayor que
+< => Menor que
+Mayor o igual que
+Menor o igual que
+<> => Diferente de
+!= => Diferente de
+=  => Asignacion
+== => Igual a (Operador de Comparacion)
+Se usan los valores o expresiones booleanas
+'''
+n1 = 0
+if n1 > 0 : #True or False
+    print("El numero", n1, "es positivo")
+elif n1 < 0:
+    print("El numero", n1, "es negativo")
+else:
+    print("El valor es cero") 
