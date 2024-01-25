27f0e9ea3f21210690f1e3c8687cfe5271d719cb
JOHN <jdante1989@gmail.com>
2024-01-24 21:14:06 -0500

    ACTUALIZACION 3

--- api_nasa1.py
+++ api_nasa1.py
@@ -8,4 +8,27 @@ Scrip description: Get and read data from NASA API about comets
 
 
 import requests
-import pygame
+
+def get_comet_data(api_key):
+    print(":::COMET INFORMATION:::")
+    url = f"?api_key={api_key}"
+
+    try:
+        #Realizar la solicitud a la API 
+        response =requests.get(url)
+        response.raise_for_status()#=> Valida si se presenta algún error en la petición
+        #Convertir la respuesta a formato JSON (JS Object Notation)
+        datos = response.json()
+
+        print(f"comet name: {datos['name']}")
+        print(f"Absolute magnitude: {datos['absolute_magnitude_h']}")
+        print(f"Estimated diameter max (KM): {datos['absolute_magnitude_h']}")
+        except requests.exceptions.RequestException as e:
+        print(f"Error al realizar la petición a la API de Nasa: {e}")
+        
+
+    api_key_nasa = 'a8u4kO1Kt9vFlS8FzbGtJbUixhOzZu38xl7LzjBq'
+    get_comet_data(api_key_nasa)
+
+
+
